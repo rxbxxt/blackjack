9d565248dc4f334f74b4df6a4d565df783cb024a
Miyake Kichiro <nshyahn1@gmail.com>
2023-03-02 19:33:13 +0200

    Init: CMake & Qt 6.4.2

---
+++ src/MainWindow.cpp
@@ -0,0 +1,16 @@
+#include "MainWindow.h"
+#include "ui_MainWindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow) {
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow() {
+    delete ui;
+}
+
+void MainWindow::on_pushButton_clicked() {
+    ui->label->setText("Test");
+}
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+
+#include "MainWindow.h"
+
+int main(int argc, char *argv[]) {
+    QApplication application(argc, argv);
+
+    MainWindow window;
+    window.show();
+
+    return application.exec();
+}

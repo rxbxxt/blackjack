36bbb45e93f21a5d8dbcd985bd5d22e5c609e090
Miyake Kichiro <nshyahn1@gmail.com>
2023-03-03 01:26:41 +0200

    Menu in progress

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,16 +1,25 @@
 #include "MainWindow.h"
-#include "ui_MainWindow.h"
 
 MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow) {
+    : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    setObjectName(MENU_WIDGET_NAME);
+    setWindowTitle(GAME_NAME);
+    setFixedSize(RESOLUTION_X, RESOLUTION_Y);
+    __setBackgroundImage();
 }
 
 MainWindow::~MainWindow() {
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked() {
-    ui->label->setText("Test");
+void MainWindow::__setBackgroundImage() {
+    QString styles;
+    QTextStream out(&styles);
+
+    out << "#" << MENU_WIDGET_NAME 
+        << " { background-image: url(:/" << BACKGROUND_TEXTURE << "); }";
+
+    setStyleSheet(styles);
 }
--- src/MainWindow.h
+++ src/MainWindow.h
@@ -2,10 +2,22 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <sstream>
+#include "ui_MainWindow.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
+
+
+#define MENU_WIDGET_NAME "menuWidget"
+#define GAME_NAME        "Blackjack"
+
+#define RESOLUTION_X     1200
+#define RESOLUTION_Y     675
+
+#define BACKGROUND_TEXTURE "images/menu_background.png"
+
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
@@ -15,10 +27,11 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_pushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
+
+    void __setBackgroundImage();
 };
 
 #endif // MAINWINDOW_H
